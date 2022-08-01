a269ee751d2e380e5e4307627c6c42c93dd51bd9
MUSBAH KELZEIH <102484194+musbahpro@users.noreply.github.com>
2022-08-02 00:23:16 +0300

    Loop  Challenge

---
+++ Loop/Loop.js
@@ -0,0 +1,12 @@
+// /*
+//   Loop Challenge
+// */  
+// let myAdmins = ["Ahmed", "Osama", "Sayed", "Stop", "Samera"];
+// let myEmployees = ["Amgad", "Samah", "Ameer", "Omar", "Othman", "Amany", "Samia", "Anwar"];
+
+//     document.write(`<div>We Have X Admins ${myAdmins.length}</div>`);
+// document.write(`<hr></hr>`);
+// console.log(myAdmins.join(" ").startsWith("A"));
+// for (let i = 0; i < myAdmins.length; i++) {
+//     const element = myAdmins[i];
+// }
